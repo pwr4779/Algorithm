b8a04a267511327c6c2d4c380ac8e9677716253f
parkwonro <park-7163@hanmail.net>
2020-07-19 00:13:20 +0900

    targetnumber add solution3(dfs)

--- Programmers/targerNumber.py
+++ Programmers/targerNumber.py
@@ -7,7 +7,7 @@ def solution1(numbers, target):
     s = list(map(sum, product(*l)))
     return s.count(target)
 
-#dfs
+#bfs
 def solution2(numbers, target):
     answer = 0
     queue = collections.deque([(0,0)])
@@ -20,8 +20,24 @@ def solution2(numbers, target):
             number = numbers[num_idx]
             queue.append((current_sum - number, num_idx + 1))
             queue.append((current_sum + number, num_idx + 1))
+    return answer
 
+
+#dfs
+def solution3(numbers, target):
+    answer = 0
+    stack = [(0,0)]
+    while stack:
+        current_sum, num_idx = stack.pop()
+        if num_idx  == len(numbers):
+            if current_sum == target:
+                answer += 1
+        else:
+            number = numbers[num_idx]
+            stack.append((current_sum - number, num_idx + 1))
+            stack.append((current_sum + number, num_idx + 1))
     return answer
 
 print(solution1([1,1,1,1,1], 3))
 print(solution2([1,1,1,1,1], 3))
+print(solution3([1,1,1,1,1], 3))
